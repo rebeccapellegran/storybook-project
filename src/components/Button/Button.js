a86059ba5528f120cbe031839dd8fa82038ea8a0
Rebecca Pellegran <rebeccapellegran@gmail.com>
2020-10-22 11:59:03 -0400

    test

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import "./Button.css";
 
 // This is a functional component - just sent up a little differently as an arrow function!
-const Button = (props) => (
-    <button>
-        {props.label}
-    </button>
-)
+const Button = (props) => {
+    console.log(props)
+    let classlist = '';
+
+    let types = ['primary', 'danger', 'success','warning']
+
+    if(types.includes(props.type)){
+        classlist+= ` button-${props.type}`
+    }
+    console.log(props.large);
+    if(props.large){
+        classlist+= ` button-large`
+    }
+    if(props.outline){
+        classlist += ` button-${props.type}-outline`   
+    }
+    return (
+        <button className={classlist}>
+            {props.label}
+        </button>
+    )
+}
 
 export default Button; 
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -4,4 +4,57 @@ import { storiesOf } from '@storybook/react';
 import Button from './Button';
 
 storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+    .add('Primary', () => <Button label="Primary" 
+    type="primary"/>) 
+
+    const style = {
+        backgroundColor: '#0069D9',
+        color: 'white',
+        borderRadius: '2px',
+        padding: '5px 25px',
+        border: '#0069D9'
+    }
+
+    const danger = {
+        backgroundColor: '#D72E3C',
+        color: 'white',
+        borderRadius: '2px',
+        padding: '5px 25px',
+        border: '#0069D9'
+    }
+    
+    const success = {
+        backgroundColor: '#53A451',
+        color: 'white',
+        borderRadius: '2px',
+        padding: '5px 25px',
+        border: '#0069D9'
+    }
+    const warning = {
+        backgroundColor: '#F6C344',
+        color: 'white',
+        borderRadius: '2px',
+        padding: '5px 25px',
+        border: '#0069D9'
+    }
+    storiesOf('Button', module)
+        .add('Primary', () => <Button
+            label="Primary"
+            style={style} />)
+        .add('Danger', () => <Button
+            label="Danger"
+            style={danger}
+        />)
+        .add('Success', () => <Button
+        label="Success"
+        style={success}
+    />)
+    .add('Warning', () => <Button
+        label="Warning"
+        style={warning}
+    />)
+    .add('Large Primary', () => <Button
+    label="Large Primary Button"
+    type="primary"
+    large
+/>)
